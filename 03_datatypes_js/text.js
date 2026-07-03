1f2f52d613ed98f1b44a711101f0c09f694e7b74
Dhirendra Kumar <dhirendrak2348@gmail.com>
2026-07-03 13:39:25 +0000

    datatypes

---
+++ 03_datatypes_js/text.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version 
+
+
+//alert(3+3) // we are using nodejs , not browser
+
+console.log(3+
+    3) // code  readability should bs high
+
+console.log("nikku");
+
+
+let name = "Dhirendra kumar"
+let age = 24
+let isLoggedIN =false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => "" , ''
+//boolean => true/false
+// null => standalone value
+// nudefined =>
+//  symbol => unique
+
+
+// object
+console.log(typeof "nikku");
+console.log(typeof null);
+console.log(typeof undefined);
