2d0127dd50251b39416804b809eaca4fb57cb421
Dhirendra Kumar <dhirendrak2348@gmail.com>
2026-07-03 14:52:38 +0000

    conversionoperation

---
+++ 04_conversionoperation.js/text.js
@@ -0,0 +1,55 @@
+let  score = 33
+
+let  score1 = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score1));
+
+let valueInNumber = Number(score1)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let age = null
+// console.log( typeof age);
+
+let  score2= undefined
+// console.log( typeof score2);
+
+let score3 = true
+let valueInNumber3 = Number(score3)
+// console.log(score3);
+// console.log(typeof score3);
+// console.log(valueInNumber3);
+
+
+
+let  name = "nikku"
+let valueInNumber2 = Number(name)
+// console.log(name);
+// console.log(valueInNumber2);
+// console.log(typeof valueInNumber2);
+
+// note
+// "33" => 33
+//"33abc" => NaN
+// true => 1; fales=>0
+
+let isLoggedIn = 1
+let booleanInLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanInLoggedIn);
+
+
+// 1 => ture
+// 0 => false
+// "" => false
+// "nikku" => true
+
+
+let someNumber =35
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+ 
